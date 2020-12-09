dc104572e4913af8c0db0d6eb9b0fc5847b8040b
EddyTam <tamsiukei@yahoo.com.hk>
2020-12-09 11:15:26 +0800

    Update iperf-dual-test-client.sh

--- iperf-dual-test-client.sh
+++ iperf-dual-test-client.sh
@@ -6,4 +6,4 @@ echo "Please input packet size:"
 read packetSize
 echo "Please input port number:"
 read port
-iperf -c 10.200.41.10 -u -i1 -t $time -p $port -l $packetSize
+iperf -c X.X.X.X -u -i1 -t $time -p $port -l $packetSize
